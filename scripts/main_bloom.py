4addd42787e4fdfc491e29b51491e32e45a994de
Erik Grenestam <egr@nationalbanken.dk>
2019-10-11 11:31:01 +0200

    Added main script to create Bloom-type indices.

---
+++ scripts/main_bloom.py
@@ -0,0 +1,44 @@
+import sys
+import os
+#hacky spyder crap
+sys.path.insert(1, 'C:\\Users\\EGR\\AppData\\Roaming\\Python\\Python37\\site-packages')
+sys.path.insert(1, 'C:\\projects\\FUI\\src')
+sys.path.insert(1, 'C:\\projects\\FUI\\hackenv')
+import json
+import pandas as pd
+import matplotlib.pyplot as plt
+import pickle
+from cycler import cycler
+import matplotlib.dates as mdates
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+
+from fui.utils import main_directory
+from fui.preprocessing import parse_raw_data
+from fui.bloom import bloom_measure, extend_dict_w2v, bloom_aggregate, plot_index
+
+if __name__ == "__main__":
+    
+    #Step 0: Parse input parameter file
+    PARAMS_PATH = 'scripts/input_params.json'
+    with open(PARAMS_PATH, encoding='utf8') as json_params:
+        params = json.load(json_params)
+    
+    #Step 1: Import Børsen articles
+    parse_raw_data(params, nrows=NROWS)
+    
+    #Step 2: Extend Bloom dict using pre-trained embeddings
+    params['bloom_extended_w2v'] = extend_dict_w2v('bloom_extended', params, n_words=10)            
+    
+    #Step 3: Get Bloom binary measure for each article using eval conditions in input_params
+    for logic in ['EandPandU','EandU','EorPandU','PandU','U']:
+        bloom_measure(params,dict_name='bloom_extended_w2v',logic=logic,start_year=2000, end_year=2019)
+    
+    #Step 4: Aggregate, write csv and plot
+    for logic in ['EandPandU','EandU','EorPandU','PandU','U']:
+        bloom_path = params['paths']['root']+params['paths']['bloom']+'bloom_extended_w2v'+'\\'+logic
+        bloom_agg = bloom_aggregate(bloom_path, params, aggregation=['M'])
+        
+        corr, fig, ax = plot_index(bloom_agg, plot_vix=True, freq='M', start_year=2012, end_year=2019)
+        print('Logic: '+logic+', Corr: '+"%.2f" % round(corr,2))
+        fig.savefig(bloom_path+'\\plot.png')
